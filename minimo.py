c9e874cd090a405231826075d375e9391b69bc14
josegdo98 <30917138+josegdo98@users.noreply.github.com>
2017-09-01 21:23:55 -0500

    Add files via upload

---
+++ minimo.py
@@ -0,0 +1,20 @@
+def minimo(arr):
+        k=arr[0]
+        for w in arr:
+            if(w<k):
+                k=w
+          print(p)
+          print(p[minimo(p)])
+       return k
+
+
+def ordenar(arr):
+    arrsort=[]
+    for k in range (len(arr)):
+        w=minimo(arr)
+        arr.remove(w)
+        arrsort.append(w)
+       print(ordenar(p))
+         
+      return arrsort
+
